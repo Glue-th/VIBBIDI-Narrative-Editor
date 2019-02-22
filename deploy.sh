49568eca412cdc8b67a028d1be63648a32c29e36
khacpv <khacpv@gmail.com>
2019-02-22 17:13:24 +0700

    deploy code

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+npm run build-prod
+
 pm2 stop 'admin-screen.vibbidi.net'
 pm2 delete 'admin-screen.vibbidi.net'
-pm2 start npm --name 'admin-screen.vibbidi.net' -- start
+pm2 start process_prod.json --no-daemon --name "admin-screen.vibbidi.net"
